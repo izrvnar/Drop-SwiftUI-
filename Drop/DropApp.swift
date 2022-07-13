cd55277ff058ef6ebb5f5be8028941b9171d099b
izrvnar <izrvnar@gmail.com>
2022-07-13 19:23:05 -0400

    Initial Commit

---
+++ Drop/DropApp.swift
@@ -0,0 +1,17 @@
+//
+//  DropApp.swift
+//  Drop
+//
+//  Created by Ivan Zrvnar on 2022-07-13.
+//
+
+import SwiftUI
+
+@main
+struct DropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
